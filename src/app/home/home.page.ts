5853974b43fc31856af34c02ed6f173dc29c6bcf
Ken Sodemann <ken@ionic.io>
2023-07-07 12:47:33 -0500

    add masks

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { MaskitoModule } from '@maskito/angular';
+import { MaskitoOptions, MaskitoElementPredicateAsync } from '@maskito/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, MaskitoModule],
 })
 export class HomePage {
   constructor() {}
+  readonly phoneMask: MaskitoOptions = {
+    mask: [
+      '+',
+      '1',
+      ' ',
+      '(',
+      /\d/,
+      /\d/,
+      /\d/,
+      ')',
+      ' ',
+      /\d/,
+      /\d/,
+      /\d/,
+      '-',
+      /\d/,
+      /\d/,
+      /\d/,
+      /\d/,
+    ],
+  };
+
+  readonly maskPredicate: MaskitoElementPredicateAsync = async (el) =>
+    (el as HTMLIonInputElement).getInputElement();
 }
